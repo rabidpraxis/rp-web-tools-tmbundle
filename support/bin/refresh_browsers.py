b4106d422ab46cfcedc45d01c33a3215a4baf505
me@kdoubleyou.com <rabidPraxis@rp.rp>
2010-11-18 12:40:07 -0800

    moved osascript out of individual script strings

--- support/bin/refresh_browsers.py
+++ support/bin/refresh_browsers.py
@@ -15,7 +15,11 @@ try:
 except KeyError:
   do_title_contains = False
 
-delay = "0.25" # delay before refreshing
+try:
+  delay = os.environ['RP_REFRESH_DELAY']
+except KeyError:
+  delay = "0.1" # delay before refreshing
+
 
 # ============================================================================
 # dict containing the Applications
@@ -23,68 +27,78 @@ delay = "0.25" # delay before refreshing
 #   command: Command to be executed from within the shell to refresh the browser
 # ============================================================================
 apps = {
-"Safari"  : {"appName" : "Safari", 
-             "command_active" : """osascript -e '
-                delay ${delay}
-                tell application "Safari" 
-                  try
-                    tell current tab of window 1 to do JavaScript "location.reload(true)"
-                  end try
-                end tell
-               ' &>/dev/null &""",
-             "command_title" : """osascript -e '
-               delay ${delay}
-               tell application "Safari"
-               	set wins_ to (tabs of windows whose name contains "${title}")
-               	repeat with win_ in wins_
-               		repeat with tab_ in win_
-               			tell tab_ to do JavaScript "location.reload(true)"
-               		end repeat
-               	end repeat
-               end tell           
-              ' &>/dev/null &"""},
-"Chrome"  : {"appName" : "Google Chrome Helper",
-             "command_active" : """osascript -e '
-                delay ${delay}
-                tell application "Google Chrome" 
-                  try
-                    tell active tab of window 1 to reload
-                  end try
-                end tell
-                ' &>/dev/null &""",
-             "command_title" : """osascript -e '
-               delay ${delay}
-               tell application "Google Chrome"
-               	set wins_ to (tabs of windows whose title contains "${title}")
-               	repeat with win_ in wins_
-               		repeat with tab_ in win_
-               			tab_ reload
-               		end repeat
-               	end repeat
-               end tell                
-             ' &>/dev/null &"""},
-"Firefox" : {"appName" : "firefox-bin",
-             "command_active" : """osascript -e '
-                 delay ${delay}
-                 tell application "Firefox"
-                 	if (count of (windows whose id is not -1)) is greater than 0 then
-                   try
-                      do shell script "echo 'BrowserReload()' | nc localhost 4242"
-                   end try
-                 end tell
-                 ' &>/dev/null &""",
-             # This is a nasty nasty mess.. I know. It works though.
-             "command_title" : """osascript -e '
-             delay ${delay}
-             tell application "Firefox"
-             	if (count of (windows whose id is not -1)) is greater than 0 then
-                try
-               		set js to "var windowEnum = Cc[\\\\\\\"@mozilla.org/appshell/window-mediator;1\\\\\\\"].getService(Ci.nsIWindowMediator).getEnumerator(\\\\\\\"\\\\\\\");while(windowEnum.hasMoreElements()) {var browsers = windowEnum.getNext().gBrowser.browsers;for (var i=0; i < browsers.length; i++) {if (browsers[i].contentTitle.indexOf(\\\\\\\"${title}\\\\\\\") != -1)browsers[i].reload();}}"
-               		do shell script "echo \\\"" & js & "\\\" | nc localhost 4242"
-                end try
-             	end if
-             end tell
-             ' &>/dev/null &"""}
+"Safari"  : {
+    "appName" : "Safari", 
+    "command_active" : 
+    """\
+      delay ${delay}
+      tell application "Safari" 
+        try
+          tell current tab of window 1 to do JavaScript "location.reload(true)"
+        end try
+      end tell
+     """,
+    "command_title" : 
+    """\
+      delay ${delay}
+      tell application "Safari"
+      	set wins_ to (tabs of windows whose name contains "${title}")
+      	repeat with win_ in wins_
+      		repeat with tab_ in win_
+      			tell tab_ to do JavaScript "location.reload(true)"
+      		end repeat
+      	end repeat
+      end tell           
+     """},
+"Chrome"  : {
+    "appName" : "Google Chrome Helper",
+    "command_active" : 
+    """\
+      delay ${delay}
+      tell application "Google Chrome" 
+        try
+          tell active tab of window 1 to reload
+        end try
+      end tell
+      """,
+    "command_title" : 
+    """\
+      delay ${delay}
+      tell application "Google Chrome"
+      	set wins_ to (tabs of windows whose title contains "${title}")
+      	repeat with win_ in wins_
+      		repeat with tab_ in win_
+      			tab_ reload
+      		end repeat
+      	end repeat
+      end tell                
+     """},
+"Firefox" : {
+    "appName" : "firefox-bin",
+    "command_active" : 
+    """\
+      delay ${delay}
+      tell application "Firefox"
+      	if (count of (windows whose id is not -1)) is greater than 0 then
+       try
+          do shell script "echo 'BrowserReload()' | nc localhost 4242"
+       end try
+      end tell
+    """,
+    # This is a nasty nasty mess.. I know. It works though.
+    "command_title" : 
+    """\
+      delay ${delay}
+      tell application "Firefox"
+      	if (count of (windows whose id is not -1)) is greater than 0 then
+        try
+       		set js to "var windowEnum = Cc[\\\\\\\"@mozilla.org/appshell/window-mediator;1\\\\\\\"].getService(Ci.nsIWindowMediator).getEnumerator(\\\\\\\"\\\\\\\");while(windowEnum.hasMoreElements()) {var browsers = windowEnum.getNext().gBrowser.browsers;for (var i=0; i < browsers.length; i++) {if (browsers[i].contentTitle.indexOf(\\\\\\\"${title}\\\\\\\") != -1)browsers[i].reload();}}"
+       		do shell script "echo \\\"" & js & "\\\" | nc localhost 4242"
+        end try
+      	end if
+      end tell
+    """}
+
 }
 
 # ====== Checks for running browser ==========================================
@@ -93,10 +107,15 @@ def check_app(arg):
              shell=True, stdout=subprocess.PIPE).communicate()
              [0])>0
 
+def process_script(command_str, replace_obj):
+  for k, v in replace_obj.iteritems():
+    command_str = command_str.replace(k, v)
+  return 'osascript -e \'' + command_str + '\' &>/dev/null &'
+
 # ====== lets go! ============================================================
 for k, v in apps.iteritems():
   if check_app(v['appName']):
     if do_title_contains:
-      subprocess.Popen(v['command_title'].replace("${title}", tab_title).replace("${delay}", delay), shell=True)
+      subprocess.Popen( process_script(v['command_title'], {"${title}":tab_title, "${delay}":delay}), shell=True)
     else:
-      subprocess.Popen(v['command_active'].replace("${delay}", delay), shell=True)
+      subprocess.Popen( process_script(v['command_active'], {"${delay}":delay}), shell=True)
